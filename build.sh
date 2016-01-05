9974ff4eb4ebfb8b3c0d4ab3ecaca92578664269
Marco Stagni <marcoponds@gmail.com>
2016-01-05 01:15:07 +0100

    moving project to electron

--- build.sh
+++ build.sh
@@ -4,11 +4,12 @@
 ./haml.sh
 
 #removing old app
-rm -r app.nw
+#rm -r app.nw
 
 #zipping new app
-zip -r -X app.nw . -x \*.git\* 
+#zip -r -X app.nw . -x \*.git\*
 
 #launching nw app
 #nw app.nw
-open app.nw
+#open app.nw
+npm start
