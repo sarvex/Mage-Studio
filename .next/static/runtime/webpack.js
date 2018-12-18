0aab5f5dd2bee2002edeb4bda1102266674c994a
Marco Stagni <mrc.stagni@gmail.com>
2018-12-18 17:16:58 +0000

    added mage-engine to magestudio

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "eea9fa67ed730ddb4395";
+/******/ 	var hotCurrentHash = "c4ab0ef8cc9a94f6472e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- app/scene/BaseScene/App.js
+++ app/scene/BaseScene/App.js
@@ -14,6 +14,8 @@ import {
     AudioEngine
 } from 'mage-engine';
 
+import { script } from './cube';
+
 export default class FirstScene extends App {
 
     loadScene = () => Promise.resolve()
@@ -22,5 +24,17 @@ export default class FirstScene extends App {
         callback();
     }
 
-    onCreate() {}
+    onCreate() {
+
+        ScriptManager.create('cube', script);
+
+        const geometry = new THREE.CubeGeometry(20, 20, 20);
+		const material = new THREE.MeshBasicMaterial({
+			color: 0x00ff00,
+			wireframe : true
+		});
+
+		const cube = new Mesh(geometry, material);
+        cube.loadScript('cube');
+    }
 }
