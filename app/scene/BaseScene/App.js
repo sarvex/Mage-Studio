32f40078332bc435c860d4abeb83f9b32c23e4a2
Marco Stagni <mrc.stagni@gmail.com>
2018-12-22 12:22:57 +0100

    transform control is working

--- app/scene/BaseScene/App.js
+++ app/scene/BaseScene/App.js
@@ -33,9 +33,51 @@ export default class FirstScene extends App {
 		});
 
 		const cube = new Mesh(geometry, material);
-        cube.loadScript('cube');
+        //cube.loadScript('cube');
 
         return cube;
+    }
+
+    onKeyPress = ({ event }) => {
+        console.log('app keypress', event, this.transform);
+        switch (event.key) {
+			case "q": // Q
+				this.transform.setSpace(this.transform.space === "local" ? "world" : "local");
+				break;
+			case "ctrl": // Ctrl
+				this.transform.setTranslationSnap(100);
+				this.transform.setRotationSnap(THREE.Math.degToRad(15));
+				break;
+			case "w": // W
+				this.transform.setMode("translate");
+				break;
+			case "e": // E
+				this.transform.setMode("rotate");
+				break;
+			case "r": // R
+				this.transform.setMode("scale");
+				break;
+			case 187:
+			case 107: // +, =, num+
+				this.transform.setSize(this.transform.size + 0.1);
+				break;
+			case 189:
+			case 109: // -, _, num-
+				this.transform.setSize(Math.max(this.transform.size - 0.1, 0.1));
+				break;
+			case 'x': // X
+				this.transform.showX = ! this.transform.showX;
+				break;
+			case 'y': // Y
+				this.transform.showY = ! this.transform.showY;
+				break;
+			case 'z': // Z
+				this.transform.showZ = ! this.transform.showZ;
+				break;
+			case 32: // Spacebar
+				this.transform.enabled = ! this.transform.enabled;
+				break;
+		}
     }
 
     onCreate() {
@@ -49,9 +91,11 @@ export default class FirstScene extends App {
 
         ControlsManager.setOrbitControl();
         ControlsManager.setTransformControl();
-        const transform = ControlsManager.getControl('transform');
+        this.transform = ControlsManager.getControl('transform');
 
-        transform.attach(cube.mesh);
+        this.transform.attach(cube.mesh);
+
+        this.enableInput();
 
         this.sceneHelper.addGrid(200, 10);
 
--- app/scene/Scene.js
+++ app/scene/Scene.js
@@ -2,15 +2,21 @@ import React from 'react';
 import './scene.scss';
 
 export default class Scene extends React.Component {
-    
+
+    constructor(props) {
+        super(props);
+
+        this.app = {};
+    }
+
     async componentDidMount() {
         const { Router } = await import('mage-engine');
         const config = await import('./config');
 
-        Router.start(config.default, '#gameContainer');
+        this.app = Router.start(config.default, '#gameContainer');
     }
 
     render() {
-        return <div id="gameContainer"></div>
+        return <div id="gameContainer" tabIndex={0}></div>
     }
 }
