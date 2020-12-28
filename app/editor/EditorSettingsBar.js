a0a05ec65f8804ca04a6645c1dec66bf47c93e68
Marco Stagni <m.stagni@elsevier.com>
2020-12-28 19:18:50 +0000

    MS-118 habemus toolbar

--- app/editor/EditorSettingsBar.js
+++ app/editor/EditorSettingsBar.js
@@ -1,6 +1,6 @@
-import { Button, Radio, Tooltip } from 'antd';
+import { Button, Radio, Tooltip, Switch, Input } from 'antd';
 import React from 'react';
-import { DragOutlined, RedoOutlined, ArrowsAltOutlined } from '@ant-design/icons';
+import { DragOutlined, RedoOutlined, ArrowsAltOutlined, PlusOutlined, MinusOutlined } from '@ant-design/icons';
 
 import './editorSettingsBar.scss';
 
@@ -14,9 +14,39 @@ const EditorSettingsBar = props => {
                     </Tooltip>
                 </li>
                 <li className='settings-list-item'>
-                    <Button className='controls-settings-radio-group-button' value="move"><DragOutlined /></Button>
-                    <Button className='controls-settings-radio-group-button' value="rotate"><RedoOutlined /></Button>
-                    <Button className='controls-settings-radio-group-button' value="scale"><ArrowsAltOutlined /></Button>
+                    <Radio.Group className='controls-settings-radio-group'>
+                        <Radio.Button className='controls-settings-radio-group-button' value="move">
+                            <DragOutlined height={12} width={12} />
+                        </Radio.Button>
+                        <Radio.Button className='controls-settings-radio-group-button' value="rotate">
+                            <RedoOutlined height={12} width={12}/>
+                        </Radio.Button>
+                        <Radio.Button className='controls-settings-radio-group-button' value="scale">
+                            <ArrowsAltOutlined height={12} width={12}/>
+                        </Radio.Button>
+                    </Radio.Group>
+                </li>
+                <li className='settings-list-item'>
+                    <div className='controls-settings-group snap-settings-group'>
+                        <label className='snap-settings-group-item'>Snap:</label>
+                        <Switch  className='snap-settings-group-item' size="small" defaultChecked />
+                        <Input
+                            size="small"
+                            placeholder="1"
+                            className='snap-settings-group-item snap-settings-group-item-text-input'
+                            prefix={<MinusOutlined/>}
+                            suffix={<PlusOutlined/>}/>
+                        {/* <Input
+                            className='snap-settings-group-item'
+                            placeholder="Enter your username"
+                            prefix={<UserOutlined className="site-form-item-icon" />}
+                            suffix={
+                                <Tooltip title="Extra information">
+                                    <InfoCircleOutlined style={{ color: 'rgba(0,0,0,.45)' }} />
+                                </Tooltip>
+                            }
+                        /> */}
+                    </div>
                 </li>
             </ul>
         </div>
