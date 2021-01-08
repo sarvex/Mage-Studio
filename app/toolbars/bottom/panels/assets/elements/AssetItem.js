ee87a4d3c5946ab7c39e09576589e391bd4c25fb
Marco Stagni <m.stagni@elsevier.com>
2021-01-08 01:02:50 +0000

    MS-121 fixed assetitem still using old antd

--- app/toolbars/bottom/panels/assets/elements/AssetItem.js
+++ app/toolbars/bottom/panels/assets/elements/AssetItem.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Tooltip, Icon } from 'antd';
+import { Tooltip } from 'antd';
+import { FileFilled } from '@ant-design/icons';
 
 const AssetItem = ({ name, type = 'file' }) => (
     <div>
         <Tooltip title={name} placement="bottom">
             <div className="asset-item folder">
-                <Icon theme="filled" className="icon" type={type} />
+                <FileFilled className="icon"/>
                 <span className="name">{name}</span>
             </div>
         </Tooltip>
